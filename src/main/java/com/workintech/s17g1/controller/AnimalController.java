a1c75defa3d5950e17b00383655da7feca7eeefb
Hülya Nergiz <akin.hulya@outlook.com>
2024-02-07 11:50:47 +0300

    adds validation methods to prevent exceptions

--- src/main/java/com/workintech/s17g1/controller/AnimalController.java
+++ src/main/java/com/workintech/s17g1/controller/AnimalController.java
@@ -1,6 +1,7 @@
 package com.workintech.s17g1.controller;
 
 import com.workintech.s17g1.entity.Animal;
+import com.workintech.s17g1.utils.ValidationUtils;
 import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,19 +26,24 @@ public class AnimalController {
     }
     @GetMapping("/{id}")
     public Animal get(@PathVariable("id") Integer id){
+        ValidationUtils.checkId(id);
         return animals.get(id);
     }
     @PostMapping
     public void addAnimal(@RequestBody Animal animal){
+        ValidationUtils.checkAnimal(animal);
         this.animals.put(animal.getId(),animal);
     }
     @PutMapping("/{id}")
     public Animal updateAnimal(@PathVariable("id") Integer existingRecordId,@RequestBody Animal newAnimal){
+        ValidationUtils.checkId(existingRecordId);
+        ValidationUtils.checkAnimal(newAnimal);
         this.animals.replace(existingRecordId,newAnimal);
         return this.animals.get(existingRecordId);
     }
     @DeleteMapping("/{id}")
     public void deleteAnimal(@PathVariable("id") Integer id){
+        ValidationUtils.checkId(id);
         this.animals.remove(id);
     }
 }
